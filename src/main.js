9e23d0f4575a540c7643d799feb8b953f60ac503
kenguruka grevisse <kenguruka24@gmail.com>
2017-05-27 09:59:58 +0300

    folders configuration example by davezuko/react-redux-starter-kit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
-import App from './containers/App';
+import App from './components/App';
 
 const render = (Component) => {
 	ReactDOM.render(
@@ -17,5 +18,5 @@ const render = (Component) => {
 render(App);
 
 if (module.hot) {
-	module.hot.accept('./containers/App', () => { render(App); });
+	module.hot.accept('./components/App', () => { render(App); });
 }
