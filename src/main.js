04415ea887658e43383686ceb115a1b6438d1faa
kenguruka grevisse <kenguruka24@gmail.com>
2017-05-29 12:04:14 +0300

    customized app folder

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef, linebreak-style */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
-import App from './components/App';
+import App from './components/app/App';
 
 const render = (Component) => {
 	ReactDOM.render(
@@ -18,5 +18,5 @@ const render = (Component) => {
 render(App);
 
 if (module.hot) {
-	module.hot.accept('./components/App', () => { render(App); });
+	module.hot.accept('./components/app/App', () => { render(App); });
 }
